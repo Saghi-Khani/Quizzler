c55eee7418e60d6ff04f511fd5737d63037912b7
Saghi Khani <saghi_kh_171@yahoo.com>
2024-06-25 15:20:46 -0400

    - First setup

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    var counter = 60
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = quizBrain.quiz[quizBrain.questionNumber + 1].text
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) 
+    {
+        let userAnswer = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnswer!)
+        
+        if(userGotItRight){
+            sender.backgroundColor = UIColor.green
+        }
+        else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+      
+        }
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(UpdateUI), userInfo: nil, repeats: false)
+
+        quizBrain.questionNumber += 1
+        UpdateUI()
+    }
+    
+    @objc func UpdateUI()
+    {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
+
