ca2f1703bdf015e149e25eea8ccd80f96f0ba3b0
Saghi Khani <saghi_kh_171@yahoo.com>
2024-06-25 21:01:14 -0400

    - Added multi choices answers

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -5,8 +5,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice3Button: UIButton!
     
     var quizBrain = QuizBrain()
     var counter = 60
@@ -15,7 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         questionLabel.text = quizBrain.quiz[quizBrain.questionNumber + 1].text
         scoreLabel.text = "Score: 0"
+        UpdateUI()
     }
+    
     
     @IBAction func answerButtonPressed(_ sender: UIButton) 
     {
@@ -30,15 +33,24 @@ class ViewController: UIViewController {
         }
         quizBrain.nextQuestion()
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(UpdateUI), userInfo: nil, repeats: false)
-        
     }
+    
     @objc func UpdateUI()
     {
         questionLabel.text = quizBrain.getQuestionText()
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+       
+        let allAnswers = quizBrain.getAllAnswers()
+        let buttons = [choice1Button, choice2Button, choice3Button]
+               
+        for (index, answer) in allAnswers.enumerated() {
+            buttons[index]?.setTitle(answer, for: .normal)
+        }
+        
+        choice1Button.backgroundColor = UIColor.clear
+        choice2Button.backgroundColor = UIColor.clear
+        choice3Button.backgroundColor = UIColor.clear
     }
 }
 
